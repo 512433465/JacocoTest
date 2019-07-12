1c5589f75968a44120360ccf19dbd8a15856c9e6
512433465 <zhangjinqiang@lvmama.com>
2019-07-12 15:21:00 +0800

    test

--- src/main/java/com/frank/springmvc/controller/HelloSpringMVC.java
+++ src/main/java/com/frank/springmvc/controller/HelloSpringMVC.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloSpringMVC {
     @RequestMapping("/hello")  
-    public String test() {  
+    public String test(Model model) {
+        model.addAttribute("message","Hello World!!!test");
         System.out.println("test"); 
         return "hello"; 
     }
