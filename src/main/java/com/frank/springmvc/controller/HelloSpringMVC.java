e5be15f795b5a0dfc40d3109db2d8d5d264ede82
512433465 <zhangjinqiang@lvmama.com>
2019-07-12 14:57:04 +0800

    test

--- src/main/java/com/frank/springmvc/controller/HelloSpringMVC.java
+++ src/main/java/com/frank/springmvc/controller/HelloSpringMVC.java
@@ -1,19 +1,19 @@
 package com.frank.springmvc.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller 
+@Controller
 public class HelloSpringMVC {
     @RequestMapping("/hello")  
     public String test() {  
         System.out.println("test"); 
         return "hello"; 
     }
-    @RequestMapping("/hello2") 
-    public String HelloWorld(Model model){  
+    @RequestMapping("/hello2")
+    public String HelloWorld(Model model){
         model.addAttribute("message","Hello World!!!spring");  
         return "hello2";  
     }  
